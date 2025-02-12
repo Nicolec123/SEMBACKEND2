f4af1df3290929ab5357178bfa8ad5e57710a9ec
Nicolec123 <danielefloripa42@gmail.com>
2025-02-12 12:26:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -364,7 +364,7 @@ async function realizarLogin() {
           document.getElementById('searchCpf').value = "";
           
          // Adiciona margem ao formulário após a pesquisa
-        document.querySelector('.form-cnpj').style.marginBottom = "15%";
+        document.querySelector('.form-cnpj').style.marginBottom = "18%";
       } catch (error) {
           console.error(error);
           resultDiv.innerHTML = `<p style="color: red;">Erro ao consultar CPFs: ${error.message}</p>`;
